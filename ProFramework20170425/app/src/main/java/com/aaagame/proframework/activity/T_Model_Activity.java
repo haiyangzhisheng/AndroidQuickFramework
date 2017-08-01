9470f11a7a7265ed9962da0ff8809f4decf4aa14
haiyangzhisheng <fengyequn@foxmail.com>
2017-08-01 09:42:14 +0800

    添加模板Activity的说明

--- ProFramework20170425/app/src/main/java/com/aaagame/proframework/activity/T_Model_Activity.java
+++ ProFramework20170425/app/src/main/java/com/aaagame/proframework/activity/T_Model_Activity.java
@@ -13,20 +13,24 @@ public class T_Model_Activity extends BaseFragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //在父类的onCreate方法中已经按照initView();initListener();initData();的顺序进行了调用，reqData()需要自己需要的时候调用
         super.onCreate(savedInstanceState);
         reqData();
     }
 
     //=============================初始化view
-        @Override  public void initView() {
+    @Override
+    public void initView() {
 
     }
 
     //=============================初始化监听
-        @Override  public void initListener() {
+    @Override
+    public void initListener() {
 
     }
 
+    //Xutils的事件监听处理，默认为onClick方法的调用{}中填入R.id.控件名，以逗号分割
     @Event(value = {})
     private void setClick(View view) {
         switch (view.getId()) {
@@ -36,12 +40,14 @@ public class T_Model_Activity extends BaseFragmentActivity {
     }
 
     //=============================初始化数据和变量
-        @Override  public void initData() {
+    @Override
+    public void initData() {
 
     }
 
     //=============================网络请求数据
-        @Override  public void reqData() {
+    @Override
+    public void reqData() {
 
     }
     //=============================其他操作
